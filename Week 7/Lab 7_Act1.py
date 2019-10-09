adc3a3aad645f545a231f693be675320d96027d7
jswood23 <jwood@swood.org>
2019-10-09 11:29:53 -0500

    Start 7.3

---
+++ Week 7/Lab 7_Act1.py
@@ -0,0 +1,15 @@
+# By submitting this assignment, all team members agree to the following:
+# “Aggies do not lie, cheat, or steal, or tolerate those who do”
+# “I have not given or received any unauthorized aid on this assignment”
+#
+# Names: Justin Compton
+# Braden Alexander
+# Josh Wood
+# Troy Hays
+# Course/Section: ENGR 102-201/216/522
+# Assignment: Lab 7 1
+# Date: 7 October 2019
+
+from math import *
+
+print('howdy world')
