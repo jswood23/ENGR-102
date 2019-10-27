e4e954e1fe0a53adcb520e25a5f70db4d5224c9a
bcalexander098 <56357120+bcalexander098@users.noreply.github.com>
2019-10-27 16:35:48 -0500

    Add files via upload

--- Week 9/Lab9_Act1.py
+++ Week 9/Lab9_Act1.py
@@ -1,1 +1,24 @@
+# For team submissions:
+# By submitting this assignment, all team members agree to the following:
+# “Aggies do not lie, cheat, or steal, or tolerate those who do”
+# “I have not given or received any unauthorized aid on this assignment”
+#
+# Names: NAME OF TEAM MEMBER 1
+# NAME OF TEAM MEMBER 2
+# NAME OF TEAM MEMBER 3
+# NAME OF TEAM MEMBER 4
+# Course/Section: ENGR 102-216
+# Assignment: THE ASSIGNMENT NUMBER
+# Date: DAY MONTH YEAR
+
+inp = open('Celsius.dat', 'r')
+out = open("Fahrenheit.dat", 'w')
+x = inp.read()
+
+for i in x.split('\n'):
+    f = (float(i) * 9 / 5) + 32
+    out.write(str(f) + '\n')
+
+inp.close()
+out.close()
 
