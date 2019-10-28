baf5177e405d3527638e98378fdad076865d98ca
jswood23 <jwood@swood.org>
2019-10-27 21:02:27 -0500

    Created files for Lab 10 Activities 1 & 2

---
+++ Week 10/Lab10_Act2.py
@@ -0,0 +1,13 @@
+# For team submissions:
+# By submitting this assignment, all team members agree to the following:
+# “Aggies do not lie, cheat, or steal, or tolerate those who do”
+# “I have not given or received any unauthorized aid on this assignment”
+#
+# Names: Josh Wood
+# # Justin Compton
+# # Braden Alexander
+# # Troy Hays
+# Course/Section: ENGR 102-216
+# Assignment: Lab 10 Activity 2
+# Date: 27 October 2019
+
