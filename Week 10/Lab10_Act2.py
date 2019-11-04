e3b4435d7b2177e439a4fca379c1c126ccdeb57c
jcompton2023 <56283196+jcompton2023@users.noreply.github.com>
2019-11-03 21:03:22 -0600

    Update Lab10_Act2.py

--- Week 10/Lab10_Act2.py
+++ Week 10/Lab10_Act2.py
@@ -11,18 +11,21 @@
 # Assignment: Lab 10 Activity 2
 # Date: 27 October 2019
 
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 
 # Part C-1 (Josh)
+
 
 def y_of_x(f, x):
     # function that returns a y value given the f and x values
     y = x ** 2 / (4 * f)
     return y
 
-x_values = numpy.linspace(-2, 2, 50)  # 1-dimensional list of values: serves as the x-axis
 
+x_values = np.linspace(-2, 2, 50)  # 1-dimensional list of values: serves as the x-axis
+
+plt.figure(1)
 plt.plot(x_values, y_of_x(2, x_values), "r-", label="f = 2", linewidth=2.0)  # red line for f = 2
 plt.plot(x_values, y_of_x(6, x_values), "b-", label="f = 6", linewidth=4.0)  # blue line for f = 6
 
@@ -32,15 +35,29 @@ plt.legend()  # show the legend matching the color of a line to the value of f
 plt.title("Lab 10 Activity 2 Part C.1")  # show the title of the plot
 plt.ylabel("y")  # label the y axis
 plt.xlabel("x")  # label the x axis
-
 plt.show()  # open a new window with the plot
 
 # Part C-2 (Braden)
+A = 2
+B = 3
+C = -11
+D = -6
 
+domain = np.linspace(-4, 4, 25)
+
+func = []
+for x in domain:
+    y = (A * x ** 3) + (B * x ** 2) + (C * x) + D
+    func.append(y)
+
+plt.figure(2)
+plt.plot(domain, func, 'o')
+plt.title('Plot of Cubic Polynomial')
+plt.ylabel('y values')
+plt.xlabel('x values')
+plt.show()
 
 # Part C-3 (Troy)
-import matplotlib.pyplot as plt
-import numpy as np
 
 # Sine graph
 xaxis = np.arange(-6, 6.5, 0.1)  # Creates x-values for sine graph
@@ -50,6 +67,7 @@ sinyaxis = np.sin(xaxis)  # Creates y-values for sine graph
 cosyaxis = np.cos(xaxis)  # Creates y-values for cosine graph
 
 # Graphs sine and cosine
+plt.figure(3)
 plt.plot(xaxis, sinyaxis, label='sin(x)')
 plt.plot(xaxis, cosyaxis, label='cos(x)', color='red')
 
