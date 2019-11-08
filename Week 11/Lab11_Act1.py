ec372a79ec5214585b7c9155c900ac51a900af86
jswood23 <56269953+jswood23@users.noreply.github.com>
2019-11-08 10:44:44 -0600

    Update Lab11_Act1.py
    Finished a and b, updated c and d to fix compatibility

--- Week 11/Lab11_Act1.py
+++ Week 11/Lab11_Act1.py
@@ -10,13 +10,34 @@
 # Assignment: Lab11 Act1
 # Date: 4 November 2019
 
+from math import *
+
 # Part a)
+
+def f_of_x(x):
+    # original function was f = x**3 + 3*x**2 + 4*x + 5
+    f = x**3 + 3*x**2 + 4*x + 5
+    return f
 
 # Part b)
 
+def deriv(x):
+    tolerance = 1e-6
+    h = 1
+    lim = 1
+    dfdx = (f_of_x(x + h) - f_of_x(x)) / h
+    while lim > tolerance:
+        h /= 2
+        dfdx1 = (f_of_x(x + h) - f_of_x(x)) / h
+        lim = abs(dfdx1 - dfdx)
+        dfdx = dfdx1
+    return round(dfdx, 2)
+
+# try print(deriv(5)) or any other number to test
+
 # Part c)
 def newton_step(num):
-    approximation = initial_guess - (F(num)/deriv(num))
+    approximation = num - (f_of_x(num) / deriv(num))
     return approximation
 
 # Part d)
@@ -31,13 +52,14 @@ def newton(initial_guess):
         next_approximation = newton_step(last_approximation)
         approximations.append(next_approximation)
         diff = last_approximation - next_approximation
-        return approximations
+    return approximations
 
 # Part e)
-F = eval(input("Enter a function in appropriate Python syntax: "))
-x0 = float(input("Enter the initial guess for a root of the function: ")
-
-print("Successive root approximations:\n", newton(x0))
+# F = eval(input("Enter a function in appropriate Python syntax: "))
+x0 = float(input("Enter the initial guess for a root of the function: "))
+newton_values = newton(x0)
+print("Successive root approximations:\n", newton_values)
+print("The most accurate value found of the root of the function is ", round(newton_values[len(newton_values) - 1], 8))
 
 # Part f)
 
