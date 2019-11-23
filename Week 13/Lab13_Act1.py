157c1c0a03b875ae94744e705618c7f849e32159
jswood23 <jwood@swood.org>
2019-11-22 21:33:09 -0600

    Started, marked errors 1 and 2. 3 more have yet to be found.

--- Week 13/Lab13_Act1.py
+++ Week 13/Lab13_Act1.py
@@ -11,46 +11,47 @@
 # Date: 20 November 2019
 
 from math import *
-from matplotlib.pyplot import *
+Vi = 10 # V
+Vf = 25 # V
 
-Vi = 10  # V
-Vf = 25  # V
-R = 30  # Ohm
-L = 25  # mH
-C = 60  # nF
-a = R / (2 * L)
-w0 = 1 / (L * C)
-wd = sqrt(a**2 - w0**2)
-delta_t = 10  # ms
+# added lines:
+Vi = float(input("Enter the initial voltage: "))
+Vf = float(input("Enter the final voltage: "))
+
+ti = 0.1e-3  # s
+R = 300 # Ohm
+L = 25e-3 # H
+C = 60e-9 # F
+a = R/(2*L)
+w0 = 1/sqrt(L*C)
+wd = sqrt(w0**2 - a**2)
+delta_t = 0.005e-3  # s
 t = []
-vc = []
 
-t += [i * 0.1 for i in range(8)]
-vc += [(Vi - Vf) * exp(-a * i) * (cos(wd * i) + (a / wd) * sin(wd * i)) + Vf for i in t]  # 1st error
-print(t)
 
-print('Time'.ljust(10), 'Voltage across Capacitor'.ljust(25), end="")
+t += [ti]
+vc=[(Vi-Vf)*(exp(-a*t[0]))*(cos(wd*t[0]+(a/wd)*(sin(wd))*t[0]))+Vf]  # 1st error: vc not defined, used 'i' instead of '0'
+print('Time'.ljust(20),'Voltage across Capacitor'.ljust(25),end="")
 print("")
-print('__________'.ljust(10), '_______________'.ljust(25), end="")
+print('__________'.ljust(20), '_______________'.ljust(25), end="")
 print("")
-for i in range(len(t)):
-    print(str(round(t[i], 2)).ljust(10), str(round(vc[i], 3)).ljust(25))  # 2nd and 3rd error
-
+print((str(round(t[0] * 10**3,3)) + " milliseconds").ljust(20),(str(round(vc[0],2)) + " V").ljust(25))  # used [0] instead of [i], multiplied t by 1000
 i = 1
-t += [i * delta_t]
-vc += [(Vi - Vf) * (exp(-a * i)) * (cos(wd * i) + (a / wd) * sin(wd * i)) + Vf]
-
-print(str(round(t[i], 1)).ljust(10), str(round(vc[i], 2)).ljust(25))
-
+t += [ti+i*delta_t]  # add values to ti
+vc+=[(Vi-Vf)*(exp(-a*t[i]))*(cos(wd*t[i]+(a/wd)*(sin(wd))*t[i]))+Vf]
+print((str(round(t[i] * 10**3,3)) + " milliseconds").ljust(20),(str(round(vc[i],2)) + " V").ljust(25))  # multiplied t by 1000
 delta_vc = vc[i] - vc[i-1]
-while delta_vc < 1e-4:
-    i += 1
-    t += [i * delta_t]
-    vc += [(Vi - Vf) * (exp(-a * i)) * (cos(wd * i) + (a / wd) * sin(wd * i)) + Vf]
-    print(str(round(t[i], 1)).ljust(10), str(round(vc[i], 2)).ljust(25))
+while delta_vc > 1e-4:  # delta_vc should be greater than 1e-4 at first, not less than
 
-plot(t, vc)
-xlabel('t (ms)')
+    i+=1  # add to i within the while loop
+    t += [ti+i*delta_t]  # lines not indented, add values to ti
+    vc+=[(Vi-Vf)*(exp(-a*t[i]))*(cos(wd*t[i]+(a/wd)*(sin(wd))*t[i]))+Vf]
+    print((str(round(t[i] * 10**3, 3)) + " milliseconds").ljust(20), (str(round(vc[i], 2)) + " V").ljust(25))  # multiplied t by 1000
+    delta_vc = vc[i] - vc[i-1]  # delta_vc did not change within the while loop
+
+from matplotlib.pyplot import *
+plot(t,vc)
+xlabel('t (s)')
 ylabel('vc (V)')
 savefig("plot.jpg")
 show()
